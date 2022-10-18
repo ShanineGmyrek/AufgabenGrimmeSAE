15e3ed6bb32f301639df573fcc87b9e1f0a65e88
ShanineGmyrek <92535968+ShanineGmyrek@users.noreply.github.com>
2022-10-18 12:08:59 +0200

    Summenrechner

---
+++ Summenrechner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Summenrechner {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type in the first Number");
+        int number1 = scanner.nextInt();
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Type in the second Number");
+        int number2 = scanner.nextInt();
+
+        int Rechnung = number1 + number2;
+
+        System.out.println("Die Summe aus " + number1 + " und " + number2 + " beträgt: " + Rechnung);
+
+    }
+}

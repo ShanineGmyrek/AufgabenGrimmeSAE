0288c5cbe2f5c6146c4b08e14f25e1668a5adba8
ShanineGmyrek <92535968+ShanineGmyrek@users.noreply.github.com>
2022-10-18 14:11:47 +0200

    Primfaktoren

---
+++ Primfaktoren.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
+public class Primfaktoren {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int primzahl = scanner.nextInt();
+        System.out.println("Bitte geben sie eine zahl ein: ");
+
+        List<Integer> zahlen = new ArrayList<Integer>();
+
+    }
+}
---
+++ SchleifenBasis.java
@@ -0,0 +1,5 @@
+public class SchleifenBasis {
+    public static void main(String[] args) {
+
+    }
+}

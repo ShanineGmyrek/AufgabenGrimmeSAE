6ec2ffa6c20520cd6195a992bbb5feddf07d1b19
ShanineGmyrek <92535968+ShanineGmyrek@users.noreply.github.com>
2022-10-20 10:52:29 +0200

    SchleifenBasis

--- SchleifenBasis.java
+++ SchleifenBasis.java
@@ -1,6 +1,8 @@
 public class SchleifenBasis {
     public static void main(String[] args) {
-
-
+        int zahl = 100;
+        for(int i = 1; i <= zahl; i++){
+            System.out.println("Zahl:" + i);
+        }
     }
 }
